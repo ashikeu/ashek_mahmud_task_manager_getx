dcf01749778bc01d5b2a328bccfe8a477281a6cc
ashikeu@gmai.com <ashikeu@gmail.com>
2025-02-19 10:26:37 +0600

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:ashek_task_manager_getx/app/utils/app_theme.dart';
+import 'package:ashek_task_manager_getx/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -7,6 +10,8 @@ import 'package:get_storage/get_storage.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform );
   await GetStorage.init(); // Initialize GetStorage
   runApp(
     GetMaterialApp(
